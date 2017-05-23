901474185854f36975f66522091f9255fe577e75
Ronaldo Dantas <ronaldo.dantas@polibrasnet.com.br>
2017-05-23 09:33:38 -0300

    Changes

--- twonaibot.py
+++ twonaibot.py
@@ -64,73 +64,87 @@ def echo_all(updates):
 def handle_updates(updates):
     global cur_cmd
     for update in updates["result"]:
-        text = update["message"]["text"]
-        chat = update["message"]["chat"]["id"]
-        members = db.get_members_delay()
-        if text == "/help":
-            cur_cmd = "hp"
-            send_message("Fala, *twospleik*! Eu rankeio os membros Twonai pelos atrasos " + 
-                "que ele cometem no setor de dev da Polibrás.\n\n Esses são meus comandos: \n\n" + 
-                " /rank - Ranking dos atrasados \n" + 
-                " /adddelay - Adicionar atraso \n" + 
-                " /addmember - Adicionar membro \n" + 
-                " /deleteall - Deletar todos os membros \n" + 
-                " /deletealldelays - Zerar os atrasos de todos os membros \n" + 
-                " /deletedelaysfrom - Zerar os atrasos de um membro \n" + 
-                " /decreasedelayfrom - Diminuir um atraso de um membro ", chat)
-        elif text == "/addmember":
-            cur_cmd = "am"
-            send_message("Digite o nome do novo membro Twonai", chat)
-        elif text == "/adddelay":
-            if members:
-                cur_cmd = "ad"
-                keyboard = build_keyboard(members)
-                send_message("Quem foi o Twonai que se atrasou?", chat, keyboard)
-            else:
-                send_message("Não existe membro Twonai cadastrado :(", chat)
-        elif text == "/rank":
-            cur_cmd = "rk"
-            msg = "Não existe membro Twonai cadastrado :("
-            if members:
-                msg = "*RANKING TWONAI ATRASADOS :D* \n\n" 
-                for member in members:
-                    msg += member.name + ": " + str(member.delay_number) + " atrasos" + "\n"
-            send_message(msg, chat)
-        elif text == "/deleteall":
-            db.deleteAll()
-            send_message("Todos os membros Twonai foram deletados :(", chat)
-        elif text == "/deletedelaysfrom":
-            cur_cmd = "deldf"
-            keyboard = build_keyboard(members)
-            send_message("Quem é o membro que terá seus atrasos zerados? :D", chat, keyboard)
-        elif text == "/decreasedelayfrom":
-            cur_cmd = "decdf"
-            keyboard = build_keyboard(members)
-            send_message("Quem é o membro que devo retirar um atraso? :D", chat, keyboard)
-        elif text == "/deletealldelays":
-            cur_cmd = "delalld"
-            db.deleteAllDelays()
-            send_message("Atrasos zerados. UI DIDI! :D", chat)
-        else:
-            if cur_cmd ==  "am":
-                db.add_member(text)
-                send_message(text + " incluido com sucesso!\n", chat)
-            elif cur_cmd ==  "deldf":
-                db.deleteDelaysFrom(text)
-                send_message(text + " esta sem atrasos, papai!\n", chat)
-            elif cur_cmd ==  "decdf":
-                db.decreaseDelaysFrom(text)
-                send_message("UAU! " + text + " deu uma reduzida nos atrasos... ;)\n", chat)
-            elif cur_cmd ==  "ad":
-                if (db.exists(text) == True):
-                    db.add_delay(text)
-                    send_message("IEEEEEI! " + text + " se lascou, papaai!", chat)
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            members = db.get_members_delay()
+            if str(text).startswith("/help"):
+                cur_cmd = "hp"
+                send_message("Fala, *twospleik*! Eu rankeio os membros Twonai pelos atrasos em reuniões " + 
+                    "que eles cometem no setor de dev da Polibrás.\n\n Esses são meus comandos: \n\n" + 
+                    " /rank - Ranking dos atrasados \n" + 
+                    " /adddelay - Adicionar atraso para um membro\n" + 
+                    " /addmember - Adicionar membro \n" + 
+                    " /deletemember - Deletar um membro \n" + 
+                    " /deleteallmembers - Deletar todos os membros \n" + 
+                    " /deletealldelays - Zerar os atrasos de todos os membros \n" + 
+                    " /deletedelaysfrom - Zerar os atrasos de um membro \n" + 
+                    " /decreasedelayfrom - Diminuir um atraso de um membro ", chat)
+            elif text.startswith("/addmember"):
+                cur_cmd = "am"
+                send_message("Digite o nome do novo membro Twonai", chat)
+            elif text.startswith("/adddelay"):
+                if members:
+                    cur_cmd = "ad"
+                    keyboard = build_keyboard(members)
+                    send_message("Quem foi o Twonai que se atrasou?", chat, keyboard)
                 else:
-                    cur_cmd = ""
-                    send_message("Oh o Douglas! Esse membro Twonai não existe.\n", chat)
-            else:
+                    send_message("Não existe membro Twonai cadastrado :(", chat)
+            elif text.startswith("/rank"):
+                cur_cmd = "rk"
+                msg = "Não existe membro Twonai cadastrado :("
+                if members:
+                    msg = "*RANKING TWONAI ATRASADOS :D* \n\n" 
+                    for member in members:
+                        msgPart = " atrasos"
+                        if member.delay_number == 1:
+                            msgPart = " atraso"
+                        msg += member.name + ": " + str(member.delay_number) + msgPart + "\n"
+                send_message(msg, chat)
+            elif text.startswith("/deleteallmembers"):
+                db.deleteAll()
+                send_message("Todos os membros Twonai foram deletados :(", chat)
+            elif text == "/deletedelaysfrom":
+                cur_cmd = "deldf"
+                keyboard = build_keyboard(members)
+                send_message("Quem é o membro que terá seus atrasos zerados? :D", chat, keyboard)
+            elif text.startswith("/decreasedelayfrom"):
+                cur_cmd = "decdf"
+                keyboard = build_keyboard(members)
+                send_message("Quem é o membro que devo retirar um atraso? :D", chat, keyboard)
+            elif text.startswith("/deletealldelays"):
+                cur_cmd = "delalld"
+                db.deleteAllDelays()
+                send_message("Atrasos zerados. UI DIDI! :D", chat)
+            elif text.startswith("/deletemember"):
+                cur_cmd = "delmb"
+                keyboard = build_keyboard(members)
+                send_message("Quem é o membro que devo dar um chute na bunda? >D", chat, keyboard)
+            elif text.startswith("/"):
                 cur_cmd = ""
                 send_message("Não existe esse comando, twonai!", chat)
+            else:
+                if cur_cmd ==  "am":
+                    db.add_member(text)
+                    send_message(text + " incluido com sucesso!\n", chat)
+                elif cur_cmd ==  "deldf":
+                    db.deleteDelaysFrom(text)
+                    send_message(text + " esta sem atrasos, papai!\n", chat)
+                elif cur_cmd ==  "decdf":
+                    db.decreaseDelaysFrom(text)
+                    send_message("UAU! " + text + " deu uma reduzida nos atrasos... ;)\n", chat)
+                elif cur_cmd == "delmb":
+                    db.delete_member(text)
+                    send_message("Ja vai tarde, " + text + "! Flw", chat)
+                elif cur_cmd ==  "ad":
+                    if (db.exists(text) == True):
+                        db.add_delay(text)
+                        send_message("IEEEEEI! " + text + " se lascou, papaai!", chat)
+                    else:
+                        cur_cmd = ""
+                        send_message("Oh o Douglas! Esse membro Twonai não existe.\n", chat)
+        except Exception as e:
+            print(e)
 
 def send_message(text, chat_id, reply_markup=None):
     text = urllib.quote_plus(text)

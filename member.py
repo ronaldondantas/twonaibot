8b7dcf162f61f7f34208bc22e1250b55c5f2ca36
Ronaldo Dantas <ronaldo.dantas@polibrasnet.com.br>
2017-06-06 07:59:21 -0300

    Correções

--- member.py
+++ member.py
@@ -1,7 +1,11 @@
 class Member:
-	def __init__(self, name, delay_number=0.0):
+	def __init__(self, name, delay_number=0.0, pendencies_number=0.0):
 		self.name = name
 		self.delay_number = delay_number
+		self.pendencies_number = pendencies_number
 
 	def setDelayNumber(self, delay_number=0.0):
 		self.delay_number = delay_number
+
+	def setPendingNumber(self, pendencies_number=0.0):
+		self.pendencies_number = pendencies_number
